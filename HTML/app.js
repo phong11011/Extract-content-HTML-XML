c4f6f9db49f0177af3b0c3f629352b79e8552e71
phong11011 <92802073+phong11011@users.noreply.github.com>
2022-04-14 09:12:17 +0700

    UPDATE

---
+++ HTML/app.js
@@ -0,0 +1,24 @@
+// Importing modules
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/', (req, res) => {
+
+    // Sending our index.html file as
+    // response. In path.join() method
+    // __dirname is the directory where
+    // our app.js file is present. In
+    // this case __dirname is the root
+    // folder of the project.
+    // res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.send("<h1>HELLO</h1>")
+});
+
+app.get('/products', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/products.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
